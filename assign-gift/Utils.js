5be9298bfd6bd61ff4fc469563f57b1f13bc4014
Erizon Encina <erizon.encina@gmail.com>
2022-10-26 17:17:32 -0300

    Clear files

--- assign-gift/Utils.js
+++
@@ -1,5 +0,0 @@
-module.exports.transformSqsMessages = (sqsMessages) => {
-    const messages = sqsMessages.Records.map((e) => e.body);
-  
-    return messages.map((e) => JSON.parse(JSON.parse(e).Message));
-}
